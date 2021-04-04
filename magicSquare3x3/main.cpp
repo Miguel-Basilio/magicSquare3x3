e38005b268c17eb9702eeb0e844188695cbc491e
Miguel-Basilio <lokoangel06@gmail.com>
2021-04-04 09:40:46 -0700

    Initial Commit

---
+++ magicSquare3x3/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  magicSquare3x3
+//
+//  Created by AB on 4/4/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
